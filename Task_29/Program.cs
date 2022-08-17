6991cc2789179d7ccc5f070eba36069a7122706a
ruslan brazhnikov <ruslan.1995mai@gmail.com>
2022-08-17 11:22:38 +0500

    Task29

---
+++ Task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите 8 элементов массива: ");
+int[] array = Convert.ToInt32(Console.ReadLine());
+
+int[] array.Length == 8;
+if (array.Length > 8)
+{
+    Console.Write("Введите 8 элементов!");
+}
+else
+{
+    Console.Write($"{array} + ,");
+}
