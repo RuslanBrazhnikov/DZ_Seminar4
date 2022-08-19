68f16b6f9e5c94f1bffd724800e05d4dece3dc7b
ruslan brazhnikov <ruslan.1995mai@gmail.com>
2022-08-19 15:21:27 +0500

    Task29

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -3,37 +3,33 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 
+
 Console.Clear();
-Console.WriteLine("Введите 1-ый элемент массива: ");
-int element1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите 2-ый элемент массива: ");
-int element2 = Convert.ToInt32(Console.ReadLine());
+int element = 8;
+int[] arr = new int[element];
 
-Console.WriteLine("Введите 3-ый элемент массива: ");
-int element3 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите 4-ый элемент массива: ");
-int element4 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите 5-ый элемент массива: ");
-int element5 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите 6-ый элемент массива: ");
-int element6 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите 7-ый элемент массива: ");
-int element7 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите 8-ый элемент массива: ");
-int element8 = Convert.ToInt32(Console.ReadLine());
-
-
-void Array()
+void FillArray(int elem, int[] array)
 {
-    Console.Write($"Ваш массив из 8 элементов -->> [{element1}, {element2}, {element3}, {element4}, {element5}, {element6}, {element7}, {element8}]");
+    for (int i = 0; i < elem; i++)
+    {
+        array[i] = new Random().Next(1, 9); 
+    }
+}
+void PrintArray(int elem_2, int[] array_2)
+{
+    Console.Write($"array = [ ");
+    for (int i = 0; i < elem_2; i++)
+    {
+        Console.Write($"{array_2[i]}, ");
+    }
+    Console.Write($"]");
 }
 
-Array();
 
-//До конца не понял как принимать на вход все элементы массива сразу.
+
+FillArray(8, arr);
+PrintArray(8, arr);
+
+
+
